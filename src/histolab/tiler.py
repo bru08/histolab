c02b74c256e39d25d8d5e13e65ec9c7738bad3c3
Bruno Papa <brown08@Brunos-MacBook-Pro.local>
2020-09-07 10:38:56 +0200

    Impl. Resrticted random tiler

--- src/histolab/tiler.py
+++ src/histolab/tiler.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 import sparse
 import cv2
+import matplotlib.pyplot as plt
 
 from .slide import Slide
 from .tile import Tile
@@ -49,6 +50,26 @@ class Tiler(Protocol):
         """
 
         return slide.biggest_tissue_box_mask
+
+    @lru_cache(maxsize=100)
+    def tissue_mask(self, slide: Slide) -> sparse._coo.core.COO:
+        """Return binary mask at level 0 of the box to consider for tiles extraction.
+
+        The mask pixels set to True will be the ones corresponding to the tissue mask.
+
+        Parameters
+        ----------
+        slide : Slide
+            The Slide from which to extract the extraction mask
+
+        Returns
+        -------
+        sparse._coo.core.COO
+            Extraction mask at level 0
+        """
+
+        return slide.tissue_mask
+
 
     @lru_cache(maxsize=100)
     def box_mask_lvl(self, slide: Slide) -> sparse._coo.core.COO:
@@ -176,8 +197,9 @@ class GridTiler(Tiler):
         slide : Slide
             Slide from which to extract the tiles
         """
+        print("start..")
         grid_tiles = self._grid_tiles_generator(slide)
-
+        print("geneator prepared...")
         tiles_counter = 0
 
         for tiles_counter, (tile, tile_wsi_coords) in enumerate(grid_tiles):
@@ -247,6 +269,7 @@ class GridTiler(Tiler):
             Iterator of tiles' CoordinatePair
         """
         box_mask_lvl = self.box_mask_lvl(slide)
+        print("mask at level: done ...")
 
         regions = regions_from_binary_mask(box_mask_lvl.todense())
         for region in regions:  # at the moment there is only one region
@@ -313,6 +336,252 @@ class GridTiler(Tiler):
         """
         return (bbox_coordinates.x_br - bbox_coordinates.x_ul) // (
             self.tile_size[0] - self.pixel_overlap
+        )
+
+
+class RestrictedGridTiler(Tiler):
+    """Extractor of tiles arranged in a grid, at the given level, with the given size.
+        Tiles more restricted to the localized tissue area.
+        Without using entire mask rescaling
+
+    Arguments
+    ---------
+    tile_size : Tuple[int, int]
+        (width, height) of the extracted tiles.
+    level : int, optional
+        Level from which extract the tiles. Default is 0.
+    check_tissue : bool, optional
+        Whether to check if the tile has enough tissue to be saved. Default is True.
+    pixel_overlap : int, optional
+       Number of overlapping pixels (for both height and width) between two adjacent
+       tiles. If negative, two adjacent tiles will be strided by the absolute value of
+       ``pixel_overlap``. Default is 0.
+    prefix : str, optional
+        Prefix to be added to the tile filename. Default is an empty string.
+    suffix : str, optional
+        Suffix to be added to the tile filename. Default is '.png'
+    """
+
+    def __init__(
+        self,
+        tile_size: Tuple[int, int],
+        level: int = 0,
+        check_tissue: bool = True,
+        pixel_overlap: int = 0,
+        prefix: str = "",
+        suffix: str = ".png",
+    ):
+        self.tile_size = tile_size
+        self.tile_size_thumb = None
+        self.level = level
+        self.check_tissue = check_tissue
+        self.pixel_overlap = pixel_overlap
+        self.prefix = prefix
+        self.suffix = suffix
+
+    @property
+    def tile_size(self) -> Tuple[int, int]:
+        return self._valid_tile_size
+
+    @tile_size.setter
+    def tile_size(self, tile_size_: Tuple[int, int]):
+        if tile_size_[0] < 1 or tile_size_[1] < 1:
+            raise ValueError(f"Tile size must be greater than 0 ({tile_size_})")
+        self._valid_tile_size = tile_size_
+
+    @property
+    def level(self) -> int:
+        return self._valid_level
+
+    @level.setter
+    def level(self, level_: int):
+        if level_ < 0:
+            raise ValueError(f"Level cannot be negative ({level_})")
+        self._valid_level = level_
+
+    def extract(self, slide: Slide):
+        """Extract tiles arranged in a grid and save them to disk, following this
+        filename pattern:
+        `{prefix}tile_{tiles_counter}_level{level}_{x_ul_wsi}-{y_ul_wsi}-{x_br_wsi}-{y_br_wsi}{suffix}`
+
+        Parameters
+        ----------
+        slide : Slide
+            Slide from which to extract the tiles
+        """
+        print("start..")
+        grid_tiles = self._grid_tiles_generator(slide)
+        print("geneator prepared...")
+        tiles_counter = 0
+
+        for tiles_counter, (tile, tile_wsi_coords) in enumerate(grid_tiles):
+            tile_filename = self._tile_filename(tile_wsi_coords, tiles_counter)
+            tile.save(tile_filename)
+            print(f"\t Tile {tiles_counter} saved: {tile_filename}")
+
+        print(f"{tiles_counter+1} Grid Tiles have been saved.")
+
+    def _grid_coordinates_from_bbox_coordinates(
+        self, bbox_coordinates: CoordinatePair, slide: Slide
+    ) -> CoordinatePair:
+        """Generate Coordinates at level 0 of grid tiles within a tissue box.
+
+        Parameters
+        ----------
+        bbox_coordinates: CoordinatePair
+            Coordinates of the tissue box from which to calculate the coordinates.
+        slide : Slide
+            Slide from which to calculate the coordinates.
+
+        Yields
+        -------
+        Iterator[CoordinatePair]
+            Iterator of tiles' CoordinatePair
+        """
+        self.tile_size_thumb = (
+            int(self.tile_size[0] * slide.level_scale_factor(self.level) / slide.tissue_mask[1]),
+            int(self.tile_size[1] * slide.level_scale_factor(self.level) / slide.tissue_mask[1])
+            )
+
+        tile_w_lvl, tile_h_lvl = self.tile_size_thumb
+        #tile_w_lvl = tile_w_lvl // 2
+        #tile_h_lvl = tile_h_lvl // 2
+
+        n_tiles_row = self._n_tiles_row(bbox_coordinates)
+        n_tiles_column = self._n_tiles_column(bbox_coordinates)
+
+        x_ul_lvl_offset = bbox_coordinates.x_ul
+        y_ul_lvl_offset = bbox_coordinates.y_ul
+
+        for i in range(n_tiles_row):
+            for j in range(n_tiles_column):
+                x_ul_lvl = x_ul_lvl_offset + tile_w_lvl * j - self.pixel_overlap
+                y_ul_lvl = y_ul_lvl_offset + tile_h_lvl * i - self.pixel_overlap
+
+                x_ul_lvl = np.clip(x_ul_lvl, x_ul_lvl_offset, None)
+                y_ul_lvl = np.clip(y_ul_lvl, y_ul_lvl_offset, None)
+
+                x_br_lvl = x_ul_lvl + tile_w_lvl
+                y_br_lvl = y_ul_lvl + tile_h_lvl
+
+                # tile_wsi_coords = scale_coordinates(
+                #     reference_coords=CoordinatePair(
+                #         x_ul_lvl, y_ul_lvl, x_br_lvl, y_br_lvl
+                #     ),
+                #     reference_size=slide.level_dimensions(level=self.level),
+                #     target_size=slide.level_dimensions(level=0),
+                # )
+                # yield tile_wsi_coords
+                res = CoordinatePair(x_ul_lvl, y_ul_lvl, x_br_lvl, y_br_lvl)
+                yield res
+
+    def _grid_coordinates_generator(self, slide: Slide) -> CoordinatePair:
+        """Generate Coordinates at level 0 of grid tiles within the tissue.
+
+        Parameters
+        ----------
+        slide : Slide
+            Slide from which to calculate the coordinates. Needed to calculate the
+            tissue area.
+
+        Yields
+        -------
+        Iterator[CoordinatePair]
+            Iterator of tiles' CoordinatePair
+        """
+        tissue_mask_small = self.tissue_mask(slide)[0]
+
+        regions = regions_from_binary_mask(tissue_mask_small)
+        for region in regions:  # at the moment there is only one region
+            bbox_coordinates = region_coordinates(region)
+            yield from self._grid_coordinates_from_bbox_coordinates(
+                bbox_coordinates, slide
+            )
+        # TODO add middleman function taking grid coordiantes, cheking on thumb mask and thhen adapting coordinates
+
+    def _grid_tiles_generator(self, slide: Slide) -> (Tile, CoordinatePair):
+        """Generator of tiles arranged in a grid.
+
+        Parameters
+        ----------
+        slide : Slide
+            Slide from which to extract the tiles
+
+        Yields
+        -------
+        Tile
+            Extracted tile
+        CoordinatePair
+            Coordinates of the slide at level 0 from which the tile has been extracted
+        """
+
+        grid_coordinates_generator = self._grid_coordinates_generator(slide)
+        for coords in grid_coordinates_generator:
+            # adapt coordinates from rescaled mask to level 0
+            coords = scale_coordinates(
+                reference_coords=coords,
+                reference_size=slide.tissue_mask[0].shape[::-1],
+                target_size=slide.level_dimensions(0)
+            )
+            try:
+                tile = slide.extract_tile(coords, self.level)
+            except ValueError:
+                continue
+            
+            if not self.check_tissue or tile.has_enough_tissue():
+                yield tile, coords
+
+    def _grid_plot(self, slide: Slide):
+        """Generate diagnostic plot to visualize tiles selected
+
+        """
+        grid_coordinates_generator = self._grid_coordinates_generator(slide)
+        thumb = np.copy(slide.resampled_array())
+        thumb_msk = self.tissue_mask(slide)[0]
+        down_factor = self.tissue_mask(slide)[1]
+        l_width = max(1, (self.tile_size_thumb[0]//(10)))
+        for x in grid_coordinates_generator:
+            if np.mean(thumb_msk[x[1]:x[3], x[0]:x[2]]) > .5:
+            
+                thumb[x[1]:x[1]+l_width,x[0]:x[2],:] = (0.,255.,0.)
+                thumb[x[3]-l_width:x[3],x[0]:x[2],:] = (0.,255,0.)
+                thumb[x[1]:x[3],x[0]:x[0]+l_width,:] = (0.,255,0.)
+                thumb[x[1]:x[3],x[2]-l_width:x[2],:] = (0.,255,0.)
+        return thumb
+
+
+    def _n_tiles_column(self, bbox_coordinates: CoordinatePair) -> int:
+        """Return the number of tiles which can be extracted in a column.
+
+        Parameters
+        ----------
+        bbox_coordinates : CoordinatePair
+            Coordinates of the tissue box
+
+        Returns
+        -------
+        int
+            Number of tiles which can be extracted in a column.
+        """
+        return (bbox_coordinates.y_br - bbox_coordinates.y_ul) // (
+            self.tile_size_thumb[1] - self.pixel_overlap
+        )
+
+    def _n_tiles_row(self, bbox_coordinates: CoordinatePair) -> int:
+        """Return the number of tiles which can be extracted in a row.
+
+        Parameters
+        ----------
+        bbox_coordinates : CoordinatePair
+            Coordinates of the tissue box
+
+        Returns
+        -------
+        int
+            Number of tiles which can be extracted in a row.
+        """
+        return (bbox_coordinates.x_br - bbox_coordinates.x_ul) // (
+            self.tile_size_thumb[0] - self.pixel_overlap
         )
 
 
